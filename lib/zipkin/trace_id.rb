c8a847f22a582976725e154969e4c9581eafed41
Indrek Juhkam <indrek@urgas.eu>
2021-01-21 11:58:29 +0200

    Fix generating trace IDs
    This is based on [OpenTelemetry][1] implementation. Previously the
    problem was that if we generated a small number then the ID length was
    less than 16 characters which zipkin was not happy with. The new
    implementation ensures there's always 16 characters even if there are
    zeros in the beginning.
    
    [1]: https://github.com/open-telemetry/opentelemetry-ruby/blob/master/api/lib/opentelemetry/trace.rb

--- lib/zipkin/trace_id.rb
+++ lib/zipkin/trace_id.rb
@@ -4,8 +4,28 @@ module Zipkin
   module TraceId
     TRACE_ID_UPPER_BOUND = 2**64
 
+    # Random number generator for generating IDs. This is an object that can
+    # respond to `#bytes` and uses the system PRNG. The current logic is
+    # compatible with Ruby 2.5 (which does not implement the `Random.bytes`
+    # class method) and with Ruby 3.0+ (which deprecates `Random::DEFAULT`).
+    # When we drop support for Ruby 2.5, this can simply be replaced with
+    # the class `Random`.
+    #
+    # @return [#bytes]
+    RANDOM = Random.respond_to?(:bytes) ? Random : Random::DEFAULT
+
+    # An invalid trace identifier, an 8-byte string with all zero bytes.
+    INVALID_TRACE_ID = ("\0" * 8).b
+
+    # Generates a valid trace identifier, an 8-byte string with at least one
+    # non-zero byte.
+    #
+    # @return [String] a valid trace ID.
     def self.generate
-      rand(TRACE_ID_UPPER_BOUND).to_s(16)
+      loop do
+        id = RANDOM.bytes(8)
+        return id.unpack1('H*') if id != INVALID_TRACE_ID
+      end
     end
   end
 end
